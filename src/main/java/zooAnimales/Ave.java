3f98d5399e98b688b1c350b12d23c3e9aeea137a
SamuelOrtizT <saortizt@unal.edu.co>
2023-09-20 09:43:10 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -23,7 +23,7 @@ public class Ave extends Animal{
         halcones++;
         return new Ave(nombre, edad, "montanas", genero, "cafe glorioso");
     }
-    public static Ave crearAguila (String nombre, int edad, String habitat, String genero, String colorPlumas) {
+    public static Ave crearAguila (String nombre, int edad, String genero) {
         aguilas++;
         return new Ave(nombre, edad, "montanas", genero, "blanco y amarillo");
     }
